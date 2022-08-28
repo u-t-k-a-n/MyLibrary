20d3ea0ff19ea8b3c20f64c8cb0667ad8f1baf5d
utkan <utkan131@hotmail.com>
2022-08-28 22:57:58 +0300

    Initial Author Routes

--- server.js
+++ server.js
@@ -6,14 +6,17 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
+const authorsRouter = require('./routes/authors');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false, limit: '10mb' }));
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
@@ -22,6 +25,7 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => console.log('connected to db'));
 
 app.use('/', indexRouter);
+app.use('/authors', authorsRouter);
 
 app.listen(process.env.PORT || 3000);
 
