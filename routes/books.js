09a3e50351e3eccf1f32301dfc86631365fe490e
utkan <utkan131@hotmail.com>
2022-09-01 13:58:50 +0300

    Finishing Book Routes

--- routes/books.js
+++ routes/books.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const path = require('path');
 const Book = require('../models/book');
 const Author = require('../models/author');
+const { log } = require('console');
 const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif', "image/jpg"];
 
 
 // All books route
 router.get('/', async (req, res) => {
     let query = Book.find();
-    if ( req.query.title != null && req.query.title != '') {
+    if (req.query.title != null && req.query.title != '') {
         query = query.regex('title', new RegExp(req.query.title, 'i'));
     }
     if (req.query.publishedBefore != null && req.query.publishedBefore != '') {
@@ -47,15 +48,88 @@ router.post('/', async (req, res) => {
     saveCover(book, req.body.cover);
     try {
         const newBook = await book.save();
-        // res.redirect(`/books/${newBook.id}`);
-        res.redirect(`/books`);
+        res.redirect(`/books/${newBook.id}`);
     }
     catch {
         renderNewPage(res, book, true);
     }
 });
 
+//show book route
+router.get('/:id', async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id).populate("author").exec();
+        res.render('books/show', { book: book });
+    }
+    catch {
+        res.redirect('/');
+    }
+});
+
+//edit book route
+router.get('/:id/edit', async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id);
+        renderEditPage(res, book);
+    }
+    catch {
+        res.redirect('/');
+    }
+});
+
+//update book route
+router.put('/:id', async (req, res) => {
+    let book;
+    try {
+        book = await Book.findById(req.params.id);
+        book.title = req.body.title;
+        book.author = req.body.author;
+        book.publishDate = new Date(req.body.publishDate);
+        book.pageCount = req.body.pageCount;
+        book.description = req.body.description;
+        if (req.body.cover != null && req.body.cover !== '') {
+            saveCover(book, req.body.cover);
+        }
+        await book.save();
+        res.redirect(`/books/${book.id}`);
+    }
+    catch {
+        if (book != null) {
+            renderEditPage(res, book, true);
+        }
+        else {
+            res.redirect('/');
+        }
+    }
+});
+
+//delete book route
+router.delete('/:id', async (req, res) => {
+    let book;
+    try {
+        book = await Book.findById(req.params.id);
+        await book.remove();
+        res.redirect('/books');
+    }
+    catch {
+        if (book != null) {
+            res.render('books/show', { book: book, errorMessage: 'Could not delete book' });
+        }
+        else {
+            res.redirect('/');
+        }
+    }
+})
+
 async function renderNewPage(res, book, hasError = false) {
+    renderFormPage(res, book, 'new', hasError);
+}
+
+async function renderEditPage(res, book, hasError = false) {
+    renderFormPage(res, book, 'edit', hasError);
+}
+
+async function renderFormPage(res, book, form, hasError = false) {
     try {
         const authors = await Author.find({});
         const params = {
@@ -63,9 +137,14 @@ async function renderNewPage(res, book, hasError = false) {
             authors: authors
         };
         if (hasError) {
-            params.errorMessage = 'Error creating book';
+            if (form === 'edit') {
+                params.errorMessage = 'Error Updating Book';
+            }
+            else {
+                params.errorMessage = 'Error updating book';
+            }
         }
-        res.render('books/new', params);
+        res.render(`books/${form}`, params);
     }
     catch {
         res.redirect('/books');
@@ -73,7 +152,7 @@ async function renderNewPage(res, book, hasError = false) {
 }
 
 function saveCover(book, coverEncoded) {
-    if ( coverEncoded == null) return;
+    if (coverEncoded == null) return;
     const cover = JSON.parse(coverEncoded);
     if (cover != null && imageMimeTypes.includes(cover.type)) {
         book.coverImage = new Buffer.from(cover.data, 'base64');
